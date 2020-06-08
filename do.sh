cb08203a5b726176f38356c10384fe65d8014508
sajad_sadra <45831343+sajad-sadra@users.noreply.github.com>
2020-06-08 19:37:20 +0430

    Update do.sh

--- do.sh
+++ do.sh
@@ -3,9 +3,9 @@ apt-get install redsocks
 systemctl enable redsocks
 
 curl -L "https://raw.githubusercontent.com/sajad-sadra/open_telegram_services/master/set_teleg_rules" -o /sbin/opentelegram
+chmod +x /sbin/opentelegram
 
 curl -L "https://raw.githubusercontent.com/sajad-sadra/open_telegram_services/master/redsocks.conf" -o /etc/redsocks.conf
-
 systemctl restart redsocks
 
 echo "149.154.167.220   api.telegram.org" >> /etc/hosts
